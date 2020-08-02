6d8df23e1949f80de7a0f481f34b2f28895bb875
vancouver29 <vancouver2990@gmail.com>
2020-08-02 15:48:38 +0200

    server cofiguration

--- book-rest-api-reactjs-springboot/src/main/webapp/reactjs/src/components/BookList.js
+++ book-rest-api-reactjs-springboot/src/main/webapp/reactjs/src/components/BookList.js
@@ -3,8 +3,23 @@ import React, {Component} from 'react';
 import {Card, Table} from 'react-bootstrap';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faList} from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
+
 
 export default class BookList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            books: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get("http://localhost:8081/rest/books")
+            .then(response => console.log(response.data));
+    }
+
+
     render() {
         return(
             <Card className={"border border-dark bg-dark text-white"}>
